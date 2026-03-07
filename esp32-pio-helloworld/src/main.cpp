907d5e8b5d4fed8128383a50c964dd9fd6ea0edf
ADARSH <adarshjhaa100@gmail.com>
2026-03-08 04:16:56 +0530

    platformio wifiscan and arduino wifiscan

---
+++ esp32-pio-helloworld/src/main.cpp
@@ -0,0 +1,44 @@
+#include <Arduino.h>
+#include "WiFi.h"
+
+void setup() {
+  Serial.begin(115200);
+
+  // Set WiFi to station mode and disconnect from an AP if it was previously connected
+  WiFi.mode(WIFI_STA);
+  WiFi.disconnect();
+  delay(100);
+
+  Serial.println("Setup done. Starting scan...");
+}
+
+void loop() {
+  Serial.println("Scanning for networks...");
+
+  // WiFi.scanNetworks will return the number of networks found
+  int n = WiFi.scanNetworks();
+  Serial.println("Scan complete.");
+
+  if (n == 0) {
+    Serial.println("No networks found.");
+  } else {
+    Serial.print(n);
+    Serial.println(" networks found:");
+    
+    for (int i = 0; i < n; ++i) {
+      // Print SSID and RSSI (Signal Strength) for each network found
+      Serial.print(i + 1);
+      Serial.print(": ");
+      Serial.print(WiFi.SSID(i));
+      Serial.print(" (");
+      Serial.print(WiFi.RSSI(i));
+      Serial.print(")");
+      Serial.println((WiFi.encryptionType(i) == WIFI_AUTH_OPEN) ? " " : "*");
+      delay(10);
+    }
+  }
+  Serial.println("");
+
+  // Wait 5 seconds before scanning again
+  delay(5000);
+}
